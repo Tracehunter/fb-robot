521c561a93ad8a3d79088d6b180d5a1d0d8fa291
Tracehunter <atak@live.be>
2017-04-05 16:30:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -253,7 +253,7 @@ var n = n+2;
     // the text we received.
     switch (messageText.toLowerCase()) {
 		    
-	case 'démarrer':
+	case 'Démarrer':
 	sendTextMessage(senderID,openingmessage+" "+firstName+", que puis-je faire pour vous aujourd'hui ?");
 	break;
 
